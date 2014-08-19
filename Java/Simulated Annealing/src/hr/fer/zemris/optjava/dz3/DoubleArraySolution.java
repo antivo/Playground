74d4fb513da92d938585f95241c5bdd4450a08e1
Stjepan Antivo Ivica <stjepan.antivo.ivica@gmail.com>
2014-08-19 08:49:42 +0200

    add

---
+++ Java/Simulated Annealing/src/hr/fer/zemris/optjava/dz3/DoubleArraySolution.java
@@ -0,0 +1,23 @@
+package hr.fer.zemris.optjava.dz3;
+
+import java.util.Random;
+
+public class DoubleArraySolution extends SingleObjectiveSolution {
+	double[] values;
+	
+	public DoubleArraySolution() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	DoubleArraySolution newLikeThis() {
+		return null;
+	}
+	
+	DoubleArraySolution dulicate() {
+		return null;
+	}
+	
+	void randomize(Random rand, double[] nest, double[] nesto) {
+		
+	}
+}
