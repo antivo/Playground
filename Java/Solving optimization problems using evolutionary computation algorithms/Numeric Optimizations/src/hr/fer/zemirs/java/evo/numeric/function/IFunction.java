03cab5f1ae067df7325406eae1dcc5cb13075133
Stjepan Antivo Ivica <stjepan.antivo.ivica@gmail.com>
2014-11-01 21:35:53 +0100

    deleted old package hierarchy

--- Java/Solving optimization problems using evolutionary computation algorithms/Numeric Optimizations/src/hr/fer/zemirs/java/evo/numeric/function/IFunction.java
+++
@@ -1,9 +0,0 @@
-package hr.fer.zemirs.java.evo.numeric.function;
-
-import Jama.Matrix;
-
-public interface IFunction {
-	int getVariableNum();
-	double f(Matrix vector);
-	Matrix grad(Matrix vector);
-}
--- Java/Solving optimization problems using evolutionary computation algorithms/Numeric Optimizations/src/hr/fer/zemirs/java/evo/numeric/function/IHFunction.java
+++
@@ -1,8 +0,0 @@
-package hr.fer.zemirs.java.evo.numeric.function;
-
-import Jama.Matrix;
-
-
-public interface IHFunction extends IFunction {
-	Matrix hessian(Matrix vector);
-}
